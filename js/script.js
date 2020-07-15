c2549a926b2603fa183012a52dc438323ff43e8b
asv-spb <asv.devpro@gmail.com>
2020-07-15 19:05:19 +0300

    На проверку -2

--- js/script.js
+++ js/script.js
@@ -4,25 +4,25 @@ let counter = 1;
 let minus = document.querySelector('.minus');
 
 plus.onclick = function () {
-    counter++;
-    quantity.value  = counter;
+  counter++;
+  quantity.value = counter;
 }
 minus.onclick = function () {
-    if (counter ===0) {
-        return
-    }
-    counter--;
-    quantity.value  = counter;
+  if (counter === 0) {
+    return
+  }
+  counter--;
+  quantity.value = counter;
 }
 
 let modal = document.getElementById('search');
 let btn = document.getElementById('my-btn');
 
-btn.onclick = function() {
-    modal.style.display = "flex";
+btn.onclick = function () {
+  modal.style.display = "flex";
+}
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
 }
